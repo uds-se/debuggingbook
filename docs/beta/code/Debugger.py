dce8edee8e685eddc538d43ce39970319f468815
Andreas Zeller <zeller@cispa.saarland>
2020-11-28 23:40:32 +0100

    Doc update

--- docs/beta/code/Debugger.py
+++ docs/beta/code/Debugger.py
@@ -3,7 +3,7 @@
 
 # This material is part of "The Fuzzing Book".
 # Web site: https://www.fuzzingbook.org/html/Debugger.html
-# Last change: 2020-11-27 20:15:51+01:00
+# Last change: 2020-11-28 23:33:34+01:00
 #
 #!/
 # Copyright (c) 2018-2020 CISPA, Saarland University, authors, and contributors
--- docs/beta/code/DeltaDebugger.py
+++ docs/beta/code/DeltaDebugger.py
@@ -3,7 +3,7 @@
 
 # This material is part of "The Fuzzing Book".
 # Web site: https://www.fuzzingbook.org/html/DeltaDebugger.html
-# Last change: 2020-11-28 23:24:45+01:00
+# Last change: 2020-11-28 23:35:05+01:00
 #
 #!/
 # Copyright (c) 2018-2020 CISPA, Saarland University, authors, and contributors
