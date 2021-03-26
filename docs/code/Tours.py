d62e81ef5e1ae6734f979fa97b8dc8d33346daf1
Andreas Zeller <zeller@cispa.saarland>
2021-03-26 13:04:26 +0100

    Doc update

--- docs/code/Tours.py
+++ docs/code/Tours.py
@@ -3,7 +3,7 @@
 
 # "Tours through the Book" - a chapter of "The Debugging Book"
 # Web site: https://www.debuggingbook.org/html/Tours.html
-# Last change: 2021-03-26 11:09:56+01:00
+# Last change: 2021-03-26 12:01:03+01:00
 #
 # Copyright (c) 2021 CISPA Helmholtz Center for Information Security
 # Copyright (c) 2018-2020 Saarland University, authors, and contributors
