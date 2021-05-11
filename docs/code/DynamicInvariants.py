752cfcb79386839fa2d3350c0a336ade230e4be7
Andreas Zeller <zeller@cispa.saarland>
2021-05-11 23:27:22 +0200

    Doc update

--- docs/code/DynamicInvariants.py
+++ docs/code/DynamicInvariants.py
@@ -3,7 +3,7 @@
 
 # "Mining Function Specifications" - a chapter of "The Debugging Book"
 # Web site: https://www.debuggingbook.org/html/DynamicInvariants.html
-# Last change: 2021-05-11 14:19:15+02:00
+# Last change: 2021-05-11 19:10:24+02:00
 #
 # Copyright (c) 2021 CISPA Helmholtz Center for Information Security
 # Copyright (c) 2018-2020 Saarland University, authors, and contributors
