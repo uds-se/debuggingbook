26ba89a4f5ee4b1dc3a5e80f436f4a7347e1f80d
Andreas Zeller <zeller@cispa.saarland>
2021-05-12 14:27:26 +0200

    Doc update

--- docs/beta/code/PerformanceDebugger.py
+++ docs/beta/code/PerformanceDebugger.py
@@ -3,7 +3,7 @@
 
 # "Debugging Performance Issues" - a chapter of "The Debugging Book"
 # Web site: https://www.debuggingbook.org/html/PerformanceDebugger.html
-# Last change: 2021-05-11 19:50:57+02:00
+# Last change: 2021-05-12 14:18:26+02:00
 #
 # Copyright (c) 2021 CISPA Helmholtz Center for Information Security
 # Copyright (c) 2018-2020 Saarland University, authors, and contributors
@@ -52,18 +52,18 @@ The distribution of executed time within each function can be obtained by printi
 
 >>> print(debugger)
  238   2% def remove_html_markup(s):  # type: ignore
- 239   1%     tag = False
+ 239   2%     tag = False
  240   1%     quote = False
- 241   1%     out = ""
+ 241   2%     out = ""
  242   0%
  243  17%     for c in s:
  244  15%         assert tag or not quote
  245   0%
- 246  15%         if c == '' and not quote:
+ 246  14%         if c == '' and not quote:
  249   3%             tag = False
  250   8%         elif (c == '"' or c == "'") and tag:
  251   0%             quote = not quote
- 252   9%         elif not tag:
+ 252   8%         elif not tag:
  253   4%             out = out + c
  254   0%
  255   3%     return out
@@ -144,11 +144,10 @@ if __name__ == '__main__':
 
 
 
-## Simple Python Profiling
-## -----------------------
+### Tracing Execution Profiles
 
 if __name__ == '__main__':
-    print('\n## Simple Python Profiling')
+    print('\n### Tracing Execution Profiles')
 
 
 
@@ -168,6 +167,21 @@ import cProfile
 if __name__ == '__main__':
     cProfile.run('debuggingbook_change_counter(ChangeCounter)', sort='cumulative')
 
+### Sampling Execution Profiles
+
+if __name__ == '__main__':
+    print('\n### Sampling Execution Profiles')
+
+
+
+## Improving Performance
+## ---------------------
+
+if __name__ == '__main__':
+    print('\n## Improving Performance')
+
+
+
 from .bookutils import quiz
 
 if __name__ == '__main__':
@@ -179,11 +193,11 @@ if __name__ == '__main__':
             "... is the reason why TeX is so fast",
         ], 'len("METAFONT") - len("TeX") - len("CWEB")')
 
-## How Profilers Work
-## ------------------
+## Building a Profiler
+## -------------------
 
 if __name__ == '__main__':
-    print('\n## How Profilers Work')
+    print('\n## Building a Profiler')
 
 
 
