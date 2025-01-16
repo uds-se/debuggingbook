74030383849d0632b9f28943576e3fc18b76151c
Andreas Zeller <zeller@cispa.de>
2025-01-16 11:45:44 +0100

    Doc update

--- docs/beta/code/Tracking.py
+++ docs/beta/code/Tracking.py
@@ -3,7 +3,7 @@
 
 # "Tracking Bugs" - a chapter of "The Debugging Book"
 # Web site: https://www.debuggingbook.org/html/Tracking.html
-# Last change: 2025-01-13 16:04:06+01:00
+# Last change: 2025-01-16 11:28:05+01:00
 #
 # Copyright (c) 2021-2025 CISPA Helmholtz Center for Information Security
 # Copyright (c) 2018-2020 Saarland University, authors, and contributors
